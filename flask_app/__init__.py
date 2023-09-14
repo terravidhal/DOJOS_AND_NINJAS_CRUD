3d9fa90cdd738aba9fd5f3bb3924798c35498872
terravidhal <infiniterraetincelle@gmail.com>
2023-09-14 19:58:09 +0100

    remove app.secret_key

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,3 +1,3 @@
 from flask import Flask
 app = Flask(__name__)
-#app.secret_key = "7a63b7ac5f8a5640035460f78fc4f8e17dd303eb4b78eeb01c3cd6e53db3b43f" # python -c 'import secrets; print(secrets.token_hex())'
+#app.secret_key = "" # python -c 'import secrets; print(secrets.token_hex())'
